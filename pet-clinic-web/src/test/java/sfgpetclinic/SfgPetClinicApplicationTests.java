ae118c077efb552493271f19c595c253fa9c52fb
Daniel Ruiz Cayuela <druiz@financialforce.com>
2020-04-06 22:20:12 +0200

    Fix unit test configuration

--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,10 +1,8 @@
 package sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootConfiguration
 @SpringBootTest
 class SfgPetClinicApplicationTests {
 
