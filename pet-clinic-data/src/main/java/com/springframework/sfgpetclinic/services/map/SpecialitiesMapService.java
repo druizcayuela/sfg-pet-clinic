05a41320e2126fa2d39835c063e41dc4fbb29334
Daniel Ruiz Cayuela <druiz@financialforce.com>
2020-04-10 14:33:56 +0200

    Added Specialities Service. Closes #31

---
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/map/SpecialitiesMapService.java
@@ -0,0 +1,33 @@
+package com.springframework.sfgpetclinic.services.map;
+
+import com.springframework.sfgpetclinic.model.Speciality;
+import com.springframework.sfgpetclinic.services.SpecialitiesService;
+
+import java.util.Set;
+
+public class SpecialitiesMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
