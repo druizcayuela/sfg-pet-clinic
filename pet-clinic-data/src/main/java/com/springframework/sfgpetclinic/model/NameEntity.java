84345b062390c31bf55509bb40acb8fa090466ea
Daniel Ruiz Cayuela <druiz@financialforce.com>
2020-04-11 18:52:51 +0200

    Added Project Lombok. Closes #51

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/NameEntity.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/NameEntity.java
@@ -1,19 +1,16 @@
 package com.springframework.sfgpetclinic.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
+@Setter
+@Getter
 @MappedSuperclass
 public class NameEntity extends BaseEntity {
 
     @Column(name = "name")
     private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/PetType.java
@@ -1,10 +1,14 @@
 package com.springframework.sfgpetclinic.model;
 
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends NameEntity {
-
 }
